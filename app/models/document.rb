282c8ace78d77b94e63f50d159d2a7d96c32704a
slavosarik <slavosarik@gmail.com>
2014-07-28 12:33:03 +0200

    added elastic search

--- app/models/document.rb
+++ app/models/document.rb
@@ -15,3 +15,14 @@ end
 #
 # faraday.post('...', json.to_json)
 #
+
+
+#d = Document.first
+
+#Faraday.post("http://localhost:9200/oz/documents/#{d.id}", d.to_indexable.to_json)
+
+
+#Crz::Contract.limit(1000).each {|d| Faraday.post("http://localhost:9200/oz/documents/#{d.id}", d.to_indexable.to_json) }
+
+#v prehliadaci URL-> http://localhost:9200/oz/_search?pretty&q=content:Bratislavskom
+#http://localhost:9200/oz/_search?pretty
